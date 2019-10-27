db98143009de9ae593a333890dbaa89763edd29e
unknown <saravananmaniganga88@gmail.com>
2019-10-27 15:13:03 +0530

    firstseondcommit

---
+++ POMLatest/src/main/java/testcases/TC001_Login.java
@@ -0,0 +1,48 @@
+package testcases;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import wrappers.ProjectWrappers;
+
+public class TC001_Login extends ProjectWrappers{
+	
+	@BeforeClass
+	public void setValues(){
+		testCaseName = "TC001";
+		testDescription = "Login";
+		authors = "Babu";
+		category = "Smoke";
+		browserName = "chrome";
+		dataSheetName = "TC001";	
+	}
+	
+	@Test(dataProvider="fetchData")
+	public void loginLogOut(String uName, String pwd, String welcome){
+		new LoginPage(driver, test)
+		.typeUserName(uName)
+		.typePassword(pwd)
+		.clickLogin()
+		.verifyWelcomeMsg(welcome)
+		.ClickbysfraLink();
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
